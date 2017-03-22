2010af9fd3d469fbcc1af7acfbe0faac23defaac
金正朋也 <mhtnim1023@gmail.com>
2017-03-22 13:45:59 +0900

    add many functions into admin

--- app/admin/kisei.rb
+++ app/admin/kisei.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Kisei do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :text, :backimage
 #
 # or
 #
