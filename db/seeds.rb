d231794b6c0f39df7f558e81816975cce4ace448
金正朋也 <mhtnim1023@gmail.com>
2017-04-02 23:46:21 +0900

    change files that add .env file

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Adminuser.create!(email: ENV['ADMIN_USER_EMAIL'], password: ENV['ADMIN_USER_PASSWORD'], password_confirmation: ENV['ADMIN_USER_PASSWORD_COMFIRMATION'])
