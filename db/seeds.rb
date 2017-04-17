a6eda321472fa13926bf8fa7c942a061c3566c9b
金正朋也 <mhtnim1023@gmail.com>
2017-04-17 23:30:52 +0900

    add-another-adminusers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Adminuser.create!(email: 'cambodiachad0917@gmail.com', password: 'cambodia0917', password_confirmation: 'cambodia0917')
+# Adminuser.create!(email: 'cambodiachad0917@gmail.com', password: 'cambodia0917', password_confirmation: 'cambodia0917')
+Adminuser.create!(email: 'rinpe-edit@gmail.com', password: 'rinpe-edit', password_confirmation: 'rinpe-edit')
