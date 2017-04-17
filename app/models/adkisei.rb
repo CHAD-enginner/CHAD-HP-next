65d18a3238d8e976294c2b42cd0b0eb8161d471e
金正朋也 <mhtnim1023@gmail.com>
2017-04-17 12:47:46 +0900

    edit validation and add validation view

--- app/models/adkisei.rb
+++ app/models/adkisei.rb
@@ -2,7 +2,6 @@ class Adkisei < ApplicationRecord
   has_many :adusers
   validates :name, presence: true
   validates :text, presence: true
-  validates :backimage, presence: true
 
   mount_uploader :backimage, ImageUploader
 end
