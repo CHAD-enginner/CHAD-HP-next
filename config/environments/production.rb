a68480bab4dc8596e39f8b6a0dd7861a7302c9ed
金正朋也 <mhtnim1023@gmail.com>
2017-04-22 20:28:49 +0900

    add email setting on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,16 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'smtp.gmail.com',
-    user_name:            'cambodiachad0917@gmail.com',
-    password:             'cambodia0917',
-    authentication:       'login',
-    enable_starttls_auto: true,
-    tls: true
-
+  host = 'chadhp.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
