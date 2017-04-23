b60aa5862e87d86aecccc15bcc8f0b872d225f6e
金正朋也 <mhtnim1023@gmail.com>
2017-04-23 23:25:46 +0900

    edit email sender and use send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,10 +67,10 @@ Rails.application.configure do
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_url_options = { host: 'chadhp.herokuapp.com' }
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: 'herokuapp.com',
+  domain: 'heroku.com',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
