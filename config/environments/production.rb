b856ae5a05ea1ffc7fc010970a8c17e3768839ed
金正朋也 <mhtnim1023@gmail.com>
2017-04-23 23:52:35 +0900

    w

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-config.action_mailer.perform_deliveries = true
+# config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
 config.action_mailer.default_url_options = { host: 'https://chadhp.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
