fcc27b97b6f29d4d47349ec7cc914496eea248e0
金正朋也 <mhtnim1023@gmail.com>
2017-04-23 17:57:56 +0900

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 host = 'chadhp.herokuapp.com'
-config.action_mailer.default_url_options = { host: host }
+config.action_mailer.default_url_options = { host: "chadhp.herokuapp.com" }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
       address:        "smtp.gmail.com",
