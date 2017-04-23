59262847605c6322d35465c1f7ae4cd8e005277b
金正朋也 <mhtnim1023@gmail.com>
2017-04-23 17:40:15 +0900

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-
+config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
+host = 'chadhp.herokuapp.com'
+config.action_mailer.default_url_options = { host: host }
 config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
 config.action_mailer.smtp_settings = {
       address:        "smtp.gmail.com",
       port:           587,
