e378f780e912549e41c7d4c3b1bcffcb3fb0303a
金正朋也 <mhtnim1023@gmail.com>
2017-05-06 20:00:56 +0900

    add advise and adusers cvs files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Adminuser.create!(id: 1, email: 'cambodiachad0917@gmail.com', password: 'cambodia0917', password_confirmation: 'cambodia0917')
-# Adminuser.create!(id: 2, email: 'rinpe-edit@gmail.com', password: 'rinpe-edit', password_confirmation: 'rinpe-edit')
+# Adminuser.create!(id: 1, email: 'cambodiachad0917@gmail.com', password: 'cambodia0917', password_confirmation: 'cambodia0917')
+Adminuser.create!(id: 2, email: 'rinpe-edit@gmail.com', password: 'rinpe-edit', password_confirmation: 'rinpe-edit')
 
 
 require "csv"
 
-# adkiseis_csv = CSV.readlines("db/adkiseis.csv")
-# adkiseis_csv.shift
-# adkiseis_csv.each do |row|
-#   Adkisei.create(name: row[1], text: row[2], backimage: row[3])
-# end
+adkiseis_csv = CSV.readlines("db/adkiseis.csv")
+adkiseis_csv.shift
+adkiseis_csv.each do |row|
+  Adkisei.create(name: row[1], text: row[2], backimage: row[3])
+end
 
 adusers_csv = CSV.readlines("db/adusers.csv")
 adusers_csv.shift
