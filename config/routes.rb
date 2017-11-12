326cc2434bba2d830719cb76b4f2cf093ddf8307
金正朋也 <mhtnim1023@gmail.com>
2017-11-12 18:29:11 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,25 @@
 Rails.application.routes.draw do
   devise_for :adminusers
-  # root 'chads#index'
-  # resources :introductions, only: [:index]
-  # resources :missions, only: [:index]
   resources :contacts, only: [:new, :create]
-  # resources :actions, only: [:index]
-  # resources :trails, only: [:index]
-  # resources :members, only: [:index]
-  # resources :kisei, only: [:index] do
-  #   resources :users, only: [:index]
-  # end
+  resources :fb_login, only: %i[index]
 
   namespace :admin do
     resources :adkiseis
-  end
-
-  namespace :admin do
     resources :adusers do
       collection do
         get 'search'
       end
     end
-  end
-
-  namespace :admin do
     resources :adcontacts, only: [:index]
-  end
-
-  namespace :admin do
-    root 'addashboard#index'
-  end
-
-  namespace :admin do
     resources :adcalenders, only: [:index]
+    resources :addashboard, only: %i[index]
+    root 'fb_login#index'
   end
 
 
-
-
-  # get '/actions/sports',        to: 'sports#index'
-  # get '/actions/educations',    to: 'educations#index'
-  # get '/trails/trip',           to: 'trips#index'
-  # get '/trails/pr',             to: 'prs#index'
-  # get '/trails/fundraising',    to: 'fundraisings#index'
-  # get '/trails/crowdfunding',   to: 'crowdfundings#index'
-  # get 'trails/others',          to: 'others#index'
+  scope '/auth' do
+    get '/callback' => 'auth#callback'
+  end
 
 #------以下、admin側のパス-------#
   get '/admin/kiseis',          to: 'admin/adkiseis#index'
@@ -54,4 +29,3 @@ Rails.application.routes.draw do
   get '/admin/users/new',       to: 'admin/adusers#new'
   get '/admin/slack/post',      to: 'admin/adusers#notify_to_slack'
 end
-
